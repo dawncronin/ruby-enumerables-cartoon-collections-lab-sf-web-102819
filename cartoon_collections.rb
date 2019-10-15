df35c780db725ea5e384bebd10d4da486d357b83
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 02:36:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def summon_captain_planet(array)# code an argument here
   return map
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
+ array.find
   # Your code here
 end
 
