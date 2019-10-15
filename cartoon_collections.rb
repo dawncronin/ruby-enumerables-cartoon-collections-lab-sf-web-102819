16db497db17a313ff7203836be3cc76f7cd8f09e
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 02:29:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves(ar# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |name, i|
+    puts "#{i} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
