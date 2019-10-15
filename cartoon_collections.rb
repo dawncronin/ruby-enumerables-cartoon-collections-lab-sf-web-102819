cad3b819a55a8ec504fa703b7601bb4704055732
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 02:37:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(array)# code an argument here
  array.each do |ele|
    
    if ele.length > 4
-     return
+     return true 
+   end
+ end
+ return false
   # Your code here
 end
 
